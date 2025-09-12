cb6a13963599cd80044b3ba6ff02af006d715d7f
xbkv <one.two.o.two@icloud.com>
2025-09-12 12:22:03 +0900

    a

--- scripts/updateReadme.js
+++ scripts/updateReadme.js
@@ -125,75 +125,141 @@ const getRandomQuote = () => {
 
   const content = `\
 <div align="center">
+  <img src="https://capsule-render.vercel.app/api?type=waving&color=gradient&customColorList=6,11,20&height=200&section=header&text=tabemono&fontSize=50&fontAlign=50&fontAlignY=40&desc=Developer%20Profile&descAlign=50&descAlignY=55" alt="Header" />
+</div>
+
+<div align="center">
 
 # tabemono の GitHub プロフィール
 
 ${asciiArt}
 
-## ${currentDate} | ${currentTime}
-**残り ${daysLeft} 日で今年が終わる... 早く終わってくれ** | ${weatherInfo}
+<table>
+  <tr>
+    <td align="center">
+      <strong>${currentDate}</strong><br/>
+      <sub>現在時刻: ${currentTime}</sub>
+    </td>
+    <td align="center">
+      <strong>今年の残り日数</strong><br/>
+      <sub>${daysLeft} 日で今年が終わる... 早く終わってくれ</sub>
+    </td>
+    <td align="center">
+      <strong>現在の天気</strong><br/>
+      <sub>${weatherInfo}</sub>
+    </td>
+  </tr>
+</table>
 
 ---
 
 ## 自己紹介
 
-- **居住地:** 東京、日本 もうすぐ大阪引っ越す 
-- **年齢:** 18歳  
-- **プログラミング開始:** 2022年10月から  
-- **現在学習中:** うーん、、SNSのチートwwwww
-- **興味分野:** Web開発、AI/ML、オープンソース
-- **目標:** お金持ちになってニートになりたい！！！！！！
+<table>
+  <tr>
+    <td><strong>居住地</strong></td>
+    <td>東京、日本 もうすぐ大阪引っ越す</td>
+  </tr>
+  <tr>
+    <td><strong>年齢</strong></td>
+    <td>18歳</td>
+  </tr>
+  <tr>
+    <td><strong>プログラミング開始</strong></td>
+    <td>2022年10月から</td>
+  </tr>
+  <tr>
+    <td><strong>現在学習中</strong></td>
+    <td>うーん、、SNSのチートwwwww</td>
+  </tr>
+  <tr>
+    <td><strong>興味分野</strong></td>
+    <td>Web開発、AI/ML、オープンソース</td>
+  </tr>
+  <tr>
+    <td><strong>目標</strong></td>
+    <td>お金持ちになってニートになりたい！！！！！</td>
+  </tr>
+</table>
 
 ---
 
 ## 技術スタック
 
+<div align="center">
+
+### プログラミング言語
 ![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
-![Node.js](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)
 ![Python](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white)
 ![C](https://img.shields.io/badge/C-00599C?style=for-the-badge&logo=c&logoColor=white)
 ![C++](https://img.shields.io/badge/C++-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white)
+
+### フレームワーク・ツール
+![Node.js](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)
 ![React](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)
 ![Git](https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white)
+
+</div>
 
 ---
 
 ## GitHub 統計
 
 <div align="center">
-  <img src="https://github-readme-stats.vercel.app/api?username=xbkv&show_icons=true&theme=tokyonight&hide_border=true" alt="GitHub Stats" width="400"/>
-  <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=xbkv&layout=compact&theme=tokyonight&hide_border=true" alt="Top Languages" width="300"/>
+  <img src="https://github-readme-stats.vercel.app/api?username=xbkv&show_icons=true&theme=tokyonight&hide_border=true&include_all_commits=true&count_private=true" alt="GitHub Stats" width="450"/>
+  <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=xbkv&layout=compact&theme=tokyonight&hide_border=true" alt="Top Languages" width="350"/>
+</div>
+
+<div align="center">
+  <img src="https://github-readme-streak-stats.herokuapp.com/?user=xbkv&theme=tokyonight&hide_border=true" alt="GitHub Streak" width="450"/>
 </div>
 
 ---
 
 ## 今日の一言
 
-${randomQuote}
+<div align="center">
+  <img src="https://readme-typing-svg.demolab.com?font=Fira+Code&size=20&duration=3000&pause=1000&color=61DAFB&center=true&vCenter=true&width=500&lines=${encodeURIComponent(randomQuote)}" alt="Typing SVG" />
+</div>
 
 ---
 
 ## 隠しメッセージ
 
-\`\`\`
+<div align="center">
+  <img src="https://img.shields.io/badge/Secret-Message-blue?style=for-the-badge" alt="Secret" />
+</div>
+
+\`\`\`text
 ${secretMessage}
 \`\`\`
-*ヒント: Base64デコードしろや！*
+
+<div align="center">
+  <sub><em>ヒント: Base64デコードしろや！</em></sub>
+</div>
 
 ---
 
 ## 連絡先
 
+<div align="center">
+
 [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/xbkv)
-[![X](https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://x.com/arca_rina)
+[![X](https://img.shields.io/badge/X-000000?style=for-the-badge&logo=x&logoColor=white)](https://x.com/arca_rina)
+
+</div>
 
 ---
 
 ### 豆知識
-このREADMEは毎日自動更新されるとおもう。多分、知らんけど
+<div align="center">
+  <img src="https://img.shields.io/badge/README-自動更新-blue?style=for-the-badge" alt="Auto Update" />
+  <br/>
+  <sub>このREADMEは毎日自動更新されるとおもう。多分、知らんけど</sub>
+</div>
 
 <div align="center">
-  <img src="https://readme-typing-svg.demolab.com?font=Fira+Code&size=18&duration=3000&pause=1000&color=61DAFB&center=true&vCenter=true&width=435&lines=訪問ありがとう！;一緒に素晴らしいものを作ろう！;コーディングし続け、学び続けよう！" alt="Typing SVG" />
+  <img src="https://capsule-render.vercel.app/api?type=waving&color=gradient&customColorList=6,11,20&height=100&section=footer" alt="Footer" />
 </div>
 
 </div>
