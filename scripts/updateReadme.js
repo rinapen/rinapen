9bfce0c03502f74018f56875b03ea2d7cfc7a3e2
xbkv <one.two.o.two@icloud.com>
2025-11-20 16:35:49 +0900

    Update

--- scripts/updateReadme.js
+++ scripts/updateReadme.js
@@ -164,7 +164,7 @@ ${progressBar}  ${yearProgress}%
 
 <div align="center">
   <img height="180em" src="https://github-readme-stats.vercel.app/api?username=rinapen&show_icons=true&theme=tokyonight&hide_border=true&include_all_commits=true&count_private=true&rank_icon=github&bg_color=0d1117" alt="GitHub Stats"/>
-  <img height="180em" src="https://github-readme-stats.vercel.app/api/top-langs/?username=rinapen&layout=compact&theme=tokyonight&hide_border=true&langs_count=8&bg_color=0d1117" alt="Top Languages"/>
+  <img height="180em" src="https://github-readme-stats.vercel.app/api/top-langs/?username=tabemono&layout=compact&theme=tokyonight&hide_border=true&langs_count=8&bg_color=0d1117" alt="Top Languages"/>
 </div>
 
 
