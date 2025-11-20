1907ab83a0e9be67e10bba385076e974904eb49a
xbkv <one.two.o.two@icloud.com>
2025-11-20 18:10:13 +0900

    Refactor' contactを整形

--- scripts/updateReadme.js
+++ scripts/updateReadme.js
@@ -166,6 +166,7 @@ ${progressLine}
 
 <div align="center">
 
+![TypeScript](https://img.shields.io/badge/-TypeScript-3178C6?style=flat-square&logo=typescript&logoColor=white)
 ![JavaScript](https://img.shields.io/badge/-JavaScript-F7DF1E?style=flat-square&logo=javascript&logoColor=black)
 ![Python](https://img.shields.io/badge/-Python-3776AB?style=flat-square&logo=python&logoColor=white)
 ![C](https://img.shields.io/badge/-C-A8B9CC?style=flat-square&logo=c&logoColor=black)
