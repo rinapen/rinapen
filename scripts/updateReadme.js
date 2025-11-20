d900b90420cb497ef238692692a13ec119dd8255
xbkv <one.two.o.two@icloud.com>
2025-11-20 16:29:39 +0900

    Delete: ReactJSを削除

--- scripts/updateReadme.js
+++ scripts/updateReadme.js
@@ -177,7 +177,6 @@ ${progressBar}  ${yearProgress}%
 ![C](https://img.shields.io/badge/-C-A8B9CC?style=flat-square&logo=c&logoColor=black)
 ![C++](https://img.shields.io/badge/-C++-00599C?style=flat-square&logo=c%2B%2B&logoColor=white)
 ![Node.js](https://img.shields.io/badge/-Node.js-339933?style=flat-square&logo=node.js&logoColor=white)
-![React](https://img.shields.io/badge/-React-61DAFB?style=flat-square&logo=react&logoColor=black)
 ![Git](https://img.shields.io/badge/-Git-F05032?style=flat-square&logo=git&logoColor=white)
 
 </div>
