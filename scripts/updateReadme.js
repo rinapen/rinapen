44a7b0ea7ddc7dcc1c5bf5462eae09a86c8a7902
xbkv <one.two.o.two@icloud.com>
2025-11-25 15:37:53 +0900

    [update]

--- scripts/updateReadme.js
+++ scripts/updateReadme.js
@@ -54,6 +54,7 @@ ${progressLine}
 </pre>
 
 <div align="center">
+
 ![TypeScript](https://img.shields.io/badge/-TypeScript-3178C6?style=flat-square&logo=typescript&logoColor=white)
 ![JavaScript](https://img.shields.io/badge/-JavaScript-F7DF1E?style=flat-square&logo=javascript&logoColor=black)
 ![Python](https://img.shields.io/badge/-Python-3776AB?style=flat-square&logo=python&logoColor=white)
